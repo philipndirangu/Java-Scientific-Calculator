2676ac5d1962ef85da48929ab27ef8610c604215
Philip Ndirangu <156826844+philipndirangu@users.noreply.github.com>
2024-02-19 23:40:09 +0300

    CalculatorUI.java

---
+++ CalculatorUI.java
@@ -0,0 +1,381 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+
+public class CalculatorUI extends JFrame implements ActionListener {
+    protected DecimalFormat df = new DecimalFormat("#,###.00");
+    protected String[] symbols = {
+            "(", ")", "π", "e", "!", "c", "+/-", "%", "÷",
+            "x²", "x³", "xʸ", "eˣ", "10ˣ", "7", "8", "9", "x",
+            "1/x", "log₁₀", "²√x", "∛x", "ʸ√ₓ", "4", "5", "6", "-",
+            "ln", "sin", "cos", "tan", "Rad", "3", "2", "1", "+",
+            "x⁻¹", "sin⁻¹", "cos⁻¹", "tan⁻¹", ".", "0", "del", "MOD", "=",
+    };
+    protected int operator = 0;
+    protected boolean sinSelected = false;
+    protected boolean cosSelected = false;
+    protected boolean tanSelected = false;
+    protected double constantValue = 0;
+
+    protected JPanel panel = new JPanel(new BorderLayout(0, 0));
+    protected JPanel btnPanel = new JPanel(new GridLayout(5, 9, 2, 2));
+    protected JButton[] btns = new JButton[symbols.length];
+    protected JTextArea screen = new JTextArea(5, 9);
+    protected double firstNum = 0, secondNum = 0;
+    protected JTextField calculatingTf = new JTextField(9);
+
+    public CalculatorUI() {
+        init();
+    }
+    public void init() {
+        setTitle("Calculator");
+        screen.setFont(new Font("San Francisco Display Light", Font.PLAIN, 20));
+        screen.setBackground(Color.BLACK);
+        panel.setBackground(Color.BLACK);
+        btnPanel.setBackground(Color.BLACK);
+        screen.setForeground(Color.WHITE);
+
+        for (int i = 0; i < symbols.length; i++) {
+            btns[i] = new JButton(symbols[i]);
+            btns[i].setOpaque(true);
+            btns[i].setBorderPainted(false);
+            btns[i].setForeground(Color.WHITE);
+            btns[i].setFont(new Font("San Francisco Display Light", Font.PLAIN, 20));
+            btns[i].addActionListener(this);
+            btnPanel.add(btns[i]);
+
+            if (symbols[i].matches("[0-9]")) {
+                btns[i].setBackground(Color.GRAY);
+            } else if (symbols[i].equals("del") || symbols[i].equals("MOD")) {
+                btns[i].setBackground(Color.GRAY);
+            } else if (symbols[i].matches("[÷x+-]")) {
+                btns[i].setBackground(new Color(240, 160, 0));
+            } else if (symbols[i].equals(".")) {
+                btns[i].setBackground(new Color(64, 64, 64));
+            } else {
+                btns[i].setBackground(new Color(64, 64, 64));
+            }
+        }
+        
+        //Removing the extra zero on the last row
+        btnPanel.remove(40);
+
+        // Add the "." button to the array
+        btns[41] = new JButton(".");
+        btns[41].setOpaque(true);
+        btns[41].setBorderPainted(false);
+        btns[41].setForeground(Color.WHITE);
+        btns[41].setFont(new Font("San Francisco Display Light", Font.PLAIN, 20));
+        btns[41].addActionListener(this);
+        btns[41].setBackground(Color.GRAY);
+        btnPanel.add(btns[41]);  // Add the button to the panel
+
+        // Adding the "=" button to the array
+        btns[40] = new JButton("=");
+        btns[40].setOpaque(true);
+        btns[40].setBorderPainted(false);
+        btns[40].setForeground(Color.WHITE);
+        btns[40].setFont(new Font("San Francisco Display Light", Font.PLAIN, 20));
+        btns[40].addActionListener(this);
+        btns[40].setBackground(new Color(240, 160, 0));
+        btnPanel.add(btns[40]);  // Add the button to the panel
+
+        // Remove the extra tan⁻¹ button
+        btnPanel.remove(42);
+
+        add(panel);
+        panel.add(screen, BorderLayout.NORTH);
+        panel.add(btnPanel, BorderLayout.CENTER);
+        panel.add(calculatingTf, BorderLayout.SOUTH);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        String cmd = e.getActionCommand().toString();
+        switch (cmd) {
+            case ".":
+                if (!screen.getText().contains(".")) {
+                    screen.append(cmd);
+                }
+                break;
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+                screen.append(cmd);
+                break;
+            case "÷":
+            case "x":
+            case "-":
+            case "+":
+            case "%":
+            case "+/-":
+            case "c":
+                performRegularOperation(cmd);
+                break;
+            case "sin":
+            case "cos":
+            case "tan":
+                selectTrigonometricFunction(cmd);
+                break;
+            case "=":
+                performCalculation();
+                break;
+            case "π":
+                constantValue = Math.PI;
+                screen.setText(Double.toString(constantValue));
+                break;
+            case "e":
+                constantValue = Math.E;
+                screen.setText(Double.toString(constantValue));
+                break;
+            case "log₁₀":
+                performLogarithm();
+                break;
+            case "x²":
+                double squareValue = Double.parseDouble(screen.getText());
+                double squareResult = Math.pow(squareValue, 2);
+                screen.setText(Double.toString(squareResult));
+                break;
+            case "x³":
+                double cubeValue = Double.parseDouble(screen.getText());
+                double cubeResult = Math.pow(cubeValue, 3);
+                screen.setText(Double.toString(cubeResult));
+                break;
+            case "xʸ":
+                firstNum = Double.parseDouble(screen.getText());
+                operator = 5;
+                screen.setText("");
+                break;
+            case "10ˣ":
+                double powTenValue = Double.parseDouble(screen.getText());
+                double powTenResult = Math.pow(10, powTenValue);
+                screen.setText(Double.toString(powTenResult));
+                break;
+            case "eˣ":
+                double expValue = Double.parseDouble(screen.getText());
+                double expResult = Math.exp(expValue);
+                screen.setText(Double.toString(expResult));
+                break;
+            case "x⁻¹":
+                performXInverse();
+                break;
+            case "1/x":
+                double reciprocalValue = Double.parseDouble(screen.getText());
+                double reciprocalResult = 1 / reciprocalValue;
+                screen.setText(Double.toString(reciprocalResult));
+                break;
+            case "²√x":
+                double sqrtValue = Double.parseDouble(screen.getText());
+                double sqrtResult = Math.sqrt(sqrtValue);
+                screen.setText(Double.toString(sqrtResult));
+                break;
+            case "∛x":
+                double cbrtValue = Double.parseDouble(screen.getText());
+                double cbrtResult = Math.cbrt(cbrtValue);
+                screen.setText(Double.toString(cbrtResult));
+                break;
+            case "ʸ√ₓ":
+                double yRootValue = Double.parseDouble(screen.getText());
+                double yRootResult = Math.pow(firstNum, 1 / yRootValue);
+                screen.setText(Double.toString(yRootResult));
+                calculatingTf.setText(firstNum + "√" + yRootValue + " = " + yRootResult);
+                break;
+            case "!":
+                performFactorial();
+                break;
+            case "MOD":
+                performModuloOperation();
+                break;
+            default:
+        }
+    }
+
+    private void performRegularOperation(String cmd) {
+        if (!screen.getText().isEmpty()) {
+            switch (cmd) {
+                case "÷":
+                case "x":
+                case "-":
+                case "+":
+                    firstNum = Double.parseDouble(screen.getText());
+                    operator = switch (cmd) {
+                        case "÷" -> 1;
+                        case "x" -> 2;
+                        case "-" -> 3;
+                        case "+" -> 4;
+                        default -> 0;
+                    };
+                    screen.setText("");
+                    break;
+                case "+/-":
+                    double value = Double.parseDouble(screen.getText());
+                    value *= -1;
+                    screen.setText(String.valueOf(value));
+                    break;
+                case "%":
+                    double percentageValue = Double.parseDouble(screen.getText());
+                    percentageValue /= 100;
+                    screen.setText(String.valueOf(percentageValue));
+                    break;
+                case "c":
+                    screen.setText("");
+                    break;
+                case "del":
+                    String currentText = screen.getText();
+                    if (!currentText.isEmpty()) {
+                        screen.setText(currentText.substring(0, currentText.length() - 1));
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void selectTrigonometricFunction(String cmd) {
+        switch (cmd) {
+            case "sin":
+                sinSelected = true;
+                break;
+            case "sin⁻¹":
+                sinSelected = true;
+                break;
+            case "cos":
+                cosSelected = true;
+                break;
+            case "cos⁻¹":
+                cosSelected = true;
+                break;
+            case "tan":
+                tanSelected = true;
+                break;
+            case "tan⁻¹":
+                tanSelected = true;
+                break;
+            default:
+        }
+        screen.setText("");
+        calculatingTf.setText(cmd);
+    }
+
+    private void performCalculation() {
+        if (!screen.getText().isEmpty()) {
+            secondNum = Double.parseDouble(screen.getText());
+            if (sinSelected || cosSelected || tanSelected) {
+                double num = Double.parseDouble(screen.getText());
+                double result = 0;
+                if (sinSelected) {
+                    result = Math.sin(Math.toRadians(num));
+                } else if (cosSelected) {
+                    result = Math.cos(Math.toRadians(num));
+                } else if (tanSelected) {
+                    result = Math.tan(Math.toRadians(num));
+                }
+                screen.setText(String.valueOf(result));
+                calculatingTf.setText(calculatingTf.getText() + "(" + num + ") = " + result);
+
+                sinSelected = false;
+                cosSelected = false;
+                tanSelected = false;
+            } else {
+                switch (operator) {
+                    case 1 -> {
+                        screen.setText(String.valueOf(firstNum / secondNum));
+                        calculatingTf.setText(String.valueOf(firstNum + "÷" + secondNum + "=" + (df.format(firstNum / secondNum))));
+                    }
+                    case 2 -> {
+                        screen.setText(String.valueOf(firstNum * secondNum));
+                        calculatingTf.setText(String.valueOf(firstNum + "x" + secondNum + "=" + (df.format(firstNum * secondNum))));
+                    }
+                    case 3 -> {
+                        screen.setText(String.valueOf(firstNum - secondNum));
+                        calculatingTf.setText(String.valueOf(firstNum + "-" + secondNum + "=" + (df.format(firstNum - secondNum))));
+                    }
+                    case 4 -> {
+                        screen.setText(String.valueOf(firstNum + secondNum));
+                        calculatingTf.setText(String.valueOf(firstNum + "+" + secondNum + "=" + (df.format(firstNum + secondNum))));
+                    }
+                    case 5 -> {
+                        double result = Math.pow(firstNum, secondNum);
+                        screen.setText(String.valueOf(result));
+                        calculatingTf.setText(String.valueOf(firstNum + "^" + secondNum + "=" + result));
+                    }
+                    case 6 -> {
+                        double result = customModuloOperation(firstNum, secondNum);
+                        screen.setText(String.valueOf(result));
+                        calculatingTf.setText(String.valueOf(firstNum + " MOD " + secondNum + "=" + result));
+                    }
+                    default -> {
+                    }
+                }
+            }
+        }
+    }
+    private void performFactorial() {
+        if (!screen.getText().isEmpty()) {
+            int num = Integer.parseInt(screen.getText());
+            long result = factorial(num);
+            screen.setText(String.valueOf(result));
+            calculatingTf.setText(num + "! = " + result);
+        }
+    }
+    private long factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+    private void performModuloOperation() {
+        if (!screen.getText().isEmpty()) {
+            firstNum = Double.parseDouble(screen.getText());
+            operator = 6;
+            screen.setText("");
+        }
+    }
+    private void performLogarithm() {
+        if (!screen.getText().isEmpty()) {
+            double logValue = Double.parseDouble(screen.getText());
+            if (logValue > 0) {
+                double lnResult = Math.log(logValue);
+                double log10Result = Math.log10(logValue);
+
+                screen.setText("ln(" + logValue + ") = " + lnResult + "\nlog(" + logValue + ") = " + log10Result);
+                calculatingTf.setText("ln(" + logValue + ") = " + lnResult);
+            } else {
+                screen.setText("Invalid input");
+            }
+        }
+    }
+    private void performXInverse() {
+        if (!screen.getText().isEmpty()) {
+            double xValue = Double.parseDouble(screen.getText());
+            if (xValue != 0) {
+                double inverseResult = 1 / xValue;
+                calculatingTf.setText("1 / " + xValue + " = " + inverseResult);
+            } else {
+                screen.setText("Cannot divide by zero");
+            }
+        }
+    }
+    private double customModuloOperation(double a, double b) {
+        return a % b;
+    }
+    public static void main(String[] args) {
+        new CalculatorUI();
+    }
+}
